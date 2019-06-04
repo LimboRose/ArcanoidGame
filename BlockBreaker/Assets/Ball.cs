c2687e54bca4055f3c048ac27147d77bb0b6d850
Rose <natspy@interia.pl>
2019-06-04 21:07:23 +0200

    Instruction 4 gemacht

--- BlockBreaker/Assets/Ball.cs
+++ BlockBreaker/Assets/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,10 @@ public class Ball : MonoBehaviour
 {
     [SerializeField] Paddle paddle1;
     Vector2 paddleToBallVector;
-
-
-
+    [SerializeField] float xpush;
+    [SerializeField] float ypush;
+    [SerializeField] bool hasStarted = false;
+    [SerializeField] Rigidbody2D rb;
 
 
 
@@ -32,6 +34,25 @@ public class Ball : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+       if(Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xpush, ypush);
+            hasStarted = true;
+            rb.simulated = true;
+        }
+    }
+
+    private void LockBallToPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
